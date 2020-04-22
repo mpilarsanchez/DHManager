e0dbcf119872e2448f24802c0c9a1a5bbd706a89
M Pilar Sanchez <sanchezmendozpilar@gmail.com>
2020-04-22 19:06:43 -0300

    commit inicial

---
+++ src/main/java/com/DHManager/DHManager/DhManagerApplication.java
@@ -0,0 +1,13 @@
+package com.DHManager.DHManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhManagerApplication.class, args);
+	}
+
+}
